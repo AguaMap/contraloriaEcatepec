faf97dd34ff175e12d66edeba6244fa68080dd19
AguaMap <mapa.sa.agua@gmail.com>
2024-05-29 12:23:09 -0600

    Add files via upload

---
+++ ecatepec/script.js
@@ -0,0 +1,13 @@
+// Resalta el botón del menú correspondiente a la página actual
+document.addEventListener('DOMContentLoaded', function () {
+  const pathname = window.location.pathname.split("/").pop();
+  const menuItems = document.querySelectorAll('.menu a');
+  
+  menuItems.forEach(item => {
+    if (item.getAttribute('href') === pathname) {
+      item.classList.add('active');
+    } else {
+      item.classList.remove('active');
+    }
+  });
+});
